8fddedcff549f27bc2e80f8cd534a6e18d0e6ca6
5eokhwan <exist909@naver.com>
2021-04-25 22:24:15 +0900

    2번-3번

--- javascript.js
+++ javascript.js
@@ -26,20 +26,45 @@ const renderData = function (text) {
     $list.innerHTML = html;
 };
 
+let $selected = null;
+function activeTap(e) {
+    if ($selected) $selected.classList.remove("active");
+    $selected = e.target.parentNode;
+    $selected.classList.add("active");
+}
+
 const showRecent = (e) => {
     e.preventDefault();
+    activeTap(e);
     readTextFile("recent.json", renderData);
 };
 const showManyView = (e) => {
     e.preventDefault();
+    activeTap(e);
     readTextFile("view.json", renderData);
 };
 const showPopularity = (e) => {
     e.preventDefault();
+    activeTap(e);
     readTextFile("popular.json", renderData);
 };
 
+//init
+$selected = $RecentBtn;
+$RecentBtn.classList.add("active");
 readTextFile("recent.json", renderData);
+
 $RecentBtn.addEventListener("click", showRecent);
 $ManyViewBtn.addEventListener("click", showManyView);
 $PopularityBtn.addEventListener("click", showPopularity);
+
+// 1boon 채널 탭 UI구현 (최근,많이본,실시간인기)
+// 각 탭을 누를때마다 해당 API를 사용하여 결과 표시
+// 각 탭이 선택되면 선택된 탭 class(active) 적용
+// 가져온 데이터를 id=list 에 노출
+// 로딩이미지 효과: 각 콘텐츠 노출시에 로딩이미지를 1초 노출후에 콘텐츠 노출
+// API에서 제목, 링크, 이미지, CP 를 적절히 표시
+// 처음 10개만 보여주고 더보기 클릭이 남은 10개 보여주기 (로딩이미지 효과도 구현)
+// JS 에러 발생하면 안됨
+// JS 네이티브 함수 사용, 최신 DOM 사용하여 구현
+// JS 함수화
